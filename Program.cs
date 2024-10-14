2ba147e596d87102b4157e3fdf6a9d4729d9d4bb
StrawmanDave <davidgb.strawman@gmail.com>
2024-10-13 18:42:40 -0700

    Greetings and description

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // David Barlow 10/13/24 lab-5 Piglatin encoder
 
-Console.WriteLine("Hello, World!");
- 
+Console.Clear();
+
+// greeting and description
+Console.WriteLine("Welcome to the Piglatin encoder anything you say will be put into Piglatin and encoded");
+Console.WriteLine("Please give me a few words or sentence");
+string phrase = Console.ReadLine();
